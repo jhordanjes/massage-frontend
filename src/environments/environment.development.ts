58280ea10cf79f52f32527c9ba3c1202222dd05e
Jhordan Oliveira <jhordanjes@gmail.com>
2023-04-25 20:39:51 -0400

    env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3003',
+  // apiUrl: 'http://localhost:3003',
+  apiUrl: 'https://sidi-massage.herokuapp.com',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3003',
+  apiUrl: 'https://sidi-massage.herokuapp.com',
 };
